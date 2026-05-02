aefe3d47168dcdc3d1f7e40034c0cf67cdf58b4f
agarwalsomya3293-lang <agarwalsomya3293@gmail.com>
2026-05-02 16:58:40 +0530

    AI Fix: Improved code stability

--- sum.py
+++ sum.py
@@ -1,12 +1,19 @@
-def sum(a,b):
- result=a-b
-return result
-print(sum(5,6))
+json
+{
+  "fixed_code": """
+def sum(a, b):
+    result = a + b
+    return result
+print(sum(5, 6))
 
-def diff(c,d):
-diff+c-d
-diff(3,4)
+def diff(c, d):
+    result = c - d
+    return result
+print(diff(3, 4))
 
-   de multi(e,f):
- mult=e-f
-multi(4,5)
+def multi(e, f):
+    result = e * f
+    return result
+print(multi(4, 5))
+"""
+}
